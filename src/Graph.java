419d97c06d93874c1edd49a979a5436af716dd1e
Daniel Kim <dkk6651@rit.edu>
2022-02-17 03:55:38 -0500

    Started implementing BFS. Changed nodeList from a regular Node array to LinkedList of Node.

--- src/Graph.java
+++ src/Graph.java
@@ -1,22 +1,24 @@
+import org.w3c.dom.NodeList;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class Graph {
-
     public static void main(String[] args){
         new Graph(args[0]);
     }
 
     public Graph(String input){
-        Node[] nodes = CreateGraph(input);
-        System.out.println(nodes.length);
-        for (Node node: nodes) {
+        LinkedList<Node> nodeList = CreateGraph(input);
+        System.out.println(nodeList.size());
+        for (Node node: nodeList) {
             System.out.println(node.toString());
         }
     }
 
-    public Node[] CreateGraph(String input){
+    public LinkedList<Node> CreateGraph(String input){
         int count;
         String currentLine;
         String[] lineArray;
@@ -24,17 +26,17 @@ public class Graph {
         try{
             BufferedReader fileReader = new BufferedReader(new FileReader(input));
             count = Integer.parseInt(fileReader.readLine());
-            Node[] nodes = new Node[count];
+            LinkedList<Node> nodes = new LinkedList<>();
             for(int i = 0; i < count; i++){
-                nodes[i] = new Node(i+1);
+                nodes.add(new Node(i+1));
             }
 
             int lineNumber = 0;
             while((currentLine = fileReader.readLine()) != null){
                 lineArray = currentLine.split(" ");
                 for(int i = 1; i < lineArray.length; i++){
-                    nodes[lineNumber].addAdj(nodes[Integer.parseInt(lineArray[i])-1]);
-                    nodes[Integer.parseInt(lineArray[i])-1].addAdj(nodes[lineNumber]);
+                    nodes.get(lineNumber).addAdj(nodes.get(Integer.parseInt(lineArray[i]) - 1));
+                    nodes.get(Integer.parseInt(lineArray[i]) - 1).addAdj(nodes.get(lineNumber));
                 }
                 lineNumber++;
             }
@@ -45,5 +47,12 @@ public class Graph {
         }
 
         return null;
+    }
+
+    public void BreadthFirstSearch(Node input){
+        int layer = 0;
+        if(input.layer == -1){
+            input.layer = layer;
+        }
     }
 }
