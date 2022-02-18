7b98e7b84dfda512e20e51cc4934cb95625aed0e
Daniel Kim <dkk6651@rit.edu>
2022-02-18 03:25:38 -0500

    Finished implementing BreadthFirstSearch and IsBipartite. BFS algorithm currently has incorrect layers at 5+ layers. IsBipartite requires more testing, but works for some test cases.

--- src/Graph.java
+++ src/Graph.java
@@ -1,11 +1,11 @@
-import org.w3c.dom.NodeList;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
 
 public class Graph {
+    int count;
+
     public static void main(String[] args){
         new Graph(args[0]);
     }
@@ -13,13 +13,28 @@ public class Graph {
     public Graph(String input){
         LinkedList<Node> nodeList = CreateGraph(input);
         System.out.println(nodeList.size());
-        for (Node node: nodeList) {
+        for(Node node: nodeList) {
             System.out.println(node.toString());
+        }
+        System.out.println("");
+        int component = 1;
+        for(Node node: nodeList){
+            if(node.layer == -1){
+                System.out.printf("connected component: %d\n", component);
+                BreadthFirstSearch(node);
+                System.out.println("");
+                if(IsBipartite(node)){
+                    System.out.println("bipartite");
+                }
+                else{
+                    System.out.println("not bipartite");
+                }
+                component++;
+            }
         }
     }
 
     public LinkedList<Node> CreateGraph(String input){
-        int count;
         String currentLine;
         String[] lineArray;
 
@@ -51,8 +66,56 @@ public class Graph {
 
     public void BreadthFirstSearch(Node input){
         int layer = 0;
-        if(input.layer == -1){
-            input.layer = layer;
+
+        Node temp = input;
+        temp.layer = layer;
+        layer++;
+        System.out.printf("%d(%d) ", temp.id, temp.layer);
+        for(Node child : temp.adj){
+            child.layer = layer;
         }
+        LinkedList<Node> queue = new LinkedList<Node>(temp.adj);
+
+        while(!queue.isEmpty()){
+            layer++;
+            temp = queue.remove();
+            System.out.printf("%d(%d) ", temp.id, temp.layer);
+            for(Node child : temp.adj){
+                if(child.layer == -1 && !queue.contains(child)){
+                    child.layer = layer;
+                    queue.add(child);
+                }
+            }
+        }
+    }
+
+    public boolean IsBipartite(Node input){
+        int[] colorArr = new int[count];
+        for(int i = 0; i < count; i++){
+            colorArr[i] = -1;
+        }
+
+        colorArr[(input.id)-1] = 1;
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.add(input);
+
+        while(queue.size() != 0){
+            Node temp = queue.poll();
+            for(Node child : temp.adj){
+                if(colorArr[temp.id - 1] == 1){
+                    if(colorArr[child.id - 1] != -1){
+                        return false;
+                    }
+                    colorArr[child.id - 1] = 0;
+                }
+                else{
+                    if(colorArr[child.id - 1] != -1){
+                        return false;
+                    }
+                    colorArr[child.id - 1] = 1;
+                }
+            }
+        }
+        return true;
     }
 }
